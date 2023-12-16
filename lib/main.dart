035476ee99947a4c4190d75c736e62e13002031a
rio <arifinrio266@gmail.com>
2023-12-16 18:16:38 +0700

    update home

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,214 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<void> newMethod() =>
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            FadeInUp(
+                duration: Duration(milliseconds: 1000),
+                child: Container(
+                  height: 220,
+                  width: double.infinity,
+                  padding: EdgeInsets.only(left: 25, right: 25, top: 40),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(50.0),
+                        bottomLeft: Radius.circular(50.0)),
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        colors: [
+                          Color.fromRGBO(255, 250, 182, 1),
+                          Color.fromRGBO(255, 239, 78, 1)
+                        ]),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      FadeInUp(
+                          duration: Duration(milliseconds: 1000),
+                          child: Image.asset(
+                            'assets/menu.png',
+                            width: 20,
+                          )),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 4,
+                            child: FadeInUp(
+                              duration: Duration(milliseconds: 1200),
+                              child: Text('Rerimpang',
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(97, 90, 90, 1))),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: FadeInUp(
+                                duration: Duration(milliseconds: 1300),
+                                child: Image.asset('assets/c.png')),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )),
+            Transform.translate(
+              offset: Offset(0, -20),
+              child: FadeInUp(
+                  duration: Duration(milliseconds: 1200),
+                  child: Container(
+                    height: 40,
+                    padding: EdgeInsets.only(left: 20, top: 8),
+                    margin: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.shade300,
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10.0))
+                        ],
+                        borderRadius: BorderRadius.circular(5.0),
+                        color: Colors.white),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          suffixIcon: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                            size: 20.0,
+                          ),
+                          border: InputBorder.none,
+                          hintText: 'Search'),
+                    ),
+                  )),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      FadeInUp(
+                        duration: Duration(milliseconds: 1200),
+                        child: Text('Tambah Resep',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Color.fromRGBO(97, 90, 90, 1))),
+                      ),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            FadeInUp(
+                                duration: Duration(milliseconds: 1200),
+                                child: MaterialButton(
+                                  elevation: 0,
+                                  highlightElevation: 0,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5)),
+                                  padding: EdgeInsets.all(10),
+                                  onPressed: () {},
+                                  color: Color.fromRGBO(251, 53, 105, 0.1),
+                                  child: Text('Add',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(251, 53, 105, 1),
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold)),
+                                )),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              height: 280,
+              width: double.infinity,
+              child: ListView(
+                padding: EdgeInsets.only(bottom: 20, left: 20),
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1300),
+                      child: makeCard(
+                          context: context,
+                          startColor: Color.fromRGBO(160, 117, 54, 1),
+                          endColor: Color.fromRGBO(157, 78, 14, 1),
+                          image: 'assets/e.png')),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1400),
+                      child: makeCard(
+                          context: context,
+                          startColor: Color.fromRGBO(203, 251, 255, 1),
+                          endColor: Color.fromRGBO(81, 223, 234, 1),
+                          image: 'assets/ef.png')),
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget makeCard({context, startColor, endColor, image}) {
+    return GestureDetector(
+      onTap: () {
+        ;
+      },
+      child: AspectRatio(
+        aspectRatio: 4 / 5,
+        child: Container(
+          margin: EdgeInsets.only(right: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(13.0),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                colors: [startColor, endColor],
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 10,
+                    offset: Offset(5, 10))
+              ]),
+          child: Padding(
+            padding: EdgeInsets.all(50),
+            child: Center(
+              child: Image.asset(image),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
