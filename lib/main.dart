adad2d2ee8d07b1c3a1d6e21ef917094ba7b6619
rio <arifinrio266@gmail.com>
2023-12-17 17:56:35 +0700

    update selebohnya

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:project_mobile_flutter/pages/Login/index.dart';
 import 'package:project_mobile_flutter/pages/beranda/index.dart';
+import 'package:project_mobile_flutter/pages/Login/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+// import 'package:project_mobile_flutter/pages/beranda/index.dart';
+import 'package:project_mobile_flutter/pages/profil/index.dart';
 
 void main() {
   runApp(MaterialApp(
---
+++ lib/pages/profil/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:project_mobile_flutter/pages/profil/widget_userprofile.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(),
+      ),
+    );
+  }
+}
