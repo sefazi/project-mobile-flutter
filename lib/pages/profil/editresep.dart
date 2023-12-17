1347b0ffe89659183d883b48a16d795a3deaf42e
rio <arifinrio266@gmail.com>
2023-12-18 01:31:51 +0700

    update menu edit profile dan menu edit bahan

---
+++ lib/pages/profil/editresep.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+class Updatebahan extends StatelessWidget {
+  const Updatebahan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Resep', style: Theme.of(context).textTheme.headline6),
+        backgroundColor: Colors.amber,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration:
+                        const InputDecoration(label: Text('Nama Penulis')),
+                  ),
+                  const SizedBox(
+                    height: 40 - 20,
+                  ),
+                  TextFormField(
+                    decoration:
+                        const InputDecoration(label: Text('Judul Resep')),
+                  ),
+                  const SizedBox(
+                    height: 40 - 20,
+                  ),
+                  TextFormField(
+                    decoration:
+                        const InputDecoration(label: Text('Bahan - bahan')),
+                  ),
+                  const SizedBox(
+                    height: 40 - 20,
+                  ),
+                  TextFormField(
+                    decoration:
+                        const InputDecoration(label: Text('Cara Pembuatan')),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.yellow,
+                          side: BorderSide.none,
+                          shape: const StadiumBorder()),
+                      child: const Text('Edit',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          side: BorderSide.none,
+                          shape: const StadiumBorder()),
+                      child: const Text('Delete',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                ],
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
